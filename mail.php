72fe4d67644acf8ed6495f5939c0f718e2841322
Lukasz Baraniak <baraniak.l@gmail.com>
2017-09-26 23:45:42 +0200

    chart-express 2day

---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+    $to = "lukasz@chart-express.pl";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $message = $_REQUEST['message'];
+
+			$od = 'MIME-Version: 1.0'."\r\n";
+			$od .= 'Content-type: text/html; charset=UTF-8'."\r\n";
+			// $od .= "From: chart-express.pl \r\n";
+
+			$tytul = "Nowe zaptanie ze strony Chart-express.pl - $what";
+			$wiadomosc = "<html>
+							<head>
+							<meta charset='UTF-8'>
+							</head>
+							<body>
+							<h2>Nowa wiadomość ze strony Chart-express.pl</h2>
+
+              <p><b>Imię:</b><br>$name</p>
+              <p><b>Email:</b><br>$from</p>
+              <p><b>Wiadomość:</b><br>$message</p>
+
+							</body>
+							</html>";
+
+
+			$send = mail($to, $tytul, $wiadomosc, $od, "-f lukasz@chart-express.pl");
+?>
